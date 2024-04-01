ad5587b289f5697f2157257a2738184f3d21cd60
Søren Gammelgaard Nielsen <sgn@logantic.com>
2024-04-01 13:16:55 +0200

    gh-pages dist

---
+++ assets/Ydelser.74e117bf.js
@@ -0,0 +1,1 @@
+import{_ as o,e as r,o as t,k as s}from"./index.6600eceb.js";const n={name:"Ydelser"};function c(a,_,p,d,f,i){const e=r("router-view");return t(),s(e)}const m=o(n,[["render",c]]);export{m as default};
